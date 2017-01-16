1268548bac2173114708c1938307d6471387e156
Rogerio A. Bueno <s2it_rbueno@uolinc.com>
2017-01-16 08:50:55 -0200

    Initial commit

---
+++ src/main/java/com/exemplo/OpenshiftDemoApplication.java
@@ -0,0 +1,12 @@
+package com.exemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftDemoApplication.class, args);
+	}
+}
